ae5a95d77b631f88b7db5203824db9c44bbc2975
Stoil Yanchev <stoilyanchev@abv.bg>
2015-04-16 18:43:48 +0300

    Add function critical_hit for attack

--- weapon.py
+++ weapon.py
@@ -1,7 +1,11 @@
+from random import randint
+
+
 class Weapon:
-    def __init__(self, name="Gloves", damage=1):
+    def __init__(self, name="Gloves", damage=1, critical_shoot_percent):
         self.name = name
         self.damage = damage
+        self.critical_shoot_percent = critical_shoot_percent
 
     def __str__(self):
         return "{} : {}".format(self.name, self.damage)
@@ -14,3 +18,13 @@ class Weapon:
 
     def __hash__(self):
         return hash(str(self))
+
+    def _set_critical_shoot_percent(self, critical_shoot_percent):
+        if critical_strike_percent >= 0 and critical_strike_percent <= 1:
+            self.critical_strike_percent = critical_strike_percent
+        else:
+            raise ValueError
+
+    def critical_hit(self):
+        roll = randint(1, 100)
+        return roll <= self.critical_shoot_percent * 100
